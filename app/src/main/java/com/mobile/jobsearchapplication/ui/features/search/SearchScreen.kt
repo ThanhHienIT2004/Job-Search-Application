18b4a0547e1526c56b33fad5983f6b37d856aaa7
Phạm Đăng Quang <140483907+QUANGNOTFUN@users.noreply.github.com>
2025-04-18 13:28:53 +0700

    fix search screen

--- app/src/main/java/com/mobile/jobsearchapplication/ui/features/search/SearchScreen.kt
+++ app/src/main/java/com/mobile/jobsearchapplication/ui/features/search/SearchScreen.kt
@@ -161,7 +161,6 @@ fun SearchScreen(navController: NavController, userId: String? = null) {
                                     .padding(horizontal = 8.dp)
                             ) {
                                 JobItem(
-                                    jobVM = viewModel(),
                                     job = job,
                                     onClick = {
                                         job.id.let {
